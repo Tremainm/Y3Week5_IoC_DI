242ce64f9c13a2d97910826fb0d7802ac29c1e67
Tremain <G00367973@atu.ie>
2024-10-22 10:09:39 +0100

    pushed master for version control

---
+++ src/main/java/ie/atu/week5_ioc_di/Week5IocDiApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_ioc_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5IocDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5IocDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5_ioc_di/Week5IocDiApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_ioc_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5IocDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
