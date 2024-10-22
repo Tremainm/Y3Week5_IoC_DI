ba71fa5629dbfc25d80af57c6fe7d32db6ff7552
Tremain <G00367973@atu.ie>
2024-10-22 11:47:35 +0100

    added validation and error handling

--- src/main/java/ie/atu/week5_ioc_di/Product.java
+++ src/main/java/ie/atu/week5_ioc_di/Product.java
@@ -1,5 +1,8 @@
 package ie.atu.week5_ioc_di;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @Min(value = 1, message = "ID must be greater than 0")
     private int id;
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Positive(message = "Value must be positive")
     private double price;
 }
